504fc450bb8cf1dff67b41d98bd8776d2eb0d761
satindersharma <47383766+satindersharma@users.noreply.github.com>
2020-08-24 18:33:12 +0530

    now project redirect to login
    added image in dashboard

--- CelecUserProject/views.py
+++ CelecUserProject/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import redirect
+# from django.shortcuts import get_object_or_404, redirect, render
+# from django.template.defaultfilters import slugify
+# from django.urls import path, reverse, reverse_lazy
 
 class Home(TemplateView):
     template_name = "dashboard.html"
+
+    def get(self, request, *args, **kwargs):
+        return redirect('login')
 
 class PrivaryPage(TemplateView):
     template_name = "privacy-policy.html"
